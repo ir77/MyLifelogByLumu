931229514b62bc5ff3a24798f0d9fe25563e5945
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2015-04-24 14:10:19 +0900

    lumu swift version

---
+++ MyLifelogByLumu/MyLifelogByLumu-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  MyLifelogByLumu-Bridging-Header.h
+//  MyLifelogByLumu
+//
+//  Created by ucuc on 4/24/15.
+//  Copyright (c) 2015 ucuc. All rights reserved.
+//
+
+#ifndef MyLifelogByLumu_MyLifelogByLumu_Bridging_Header_h
+#define MyLifelogByLumu_MyLifelogByLumu_Bridging_Header_h
+
+#import <Lumu/Lumu.h>
+#import <Lumu/LumuManager.h>
+
+#endif
--- MyLifelogByLumu/ViewController.swift
+++ MyLifelogByLumu/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, LumuManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSLog("viewDidLoad");
+        var lm = LumuManager.sharedManager()
+        lm.delegate = self
+        lm.startLumuManager()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func lumuManagerDidStartLumu() {
+        NSLog("Start");
+    }
+    
+    func lumuManagerDidRecognizeLumu() {
+        NSLog("Recognized");
+    }
+    
+    func lumuManagerDidNotRecognizeLumu() {
+        NSLog("Not recognized");
+    }
+    
+    func lumuManagerDidNotGetRecordPermission() {
+        NSLog("No record permission");
+    }
 
+    func lumuManagerDidReceiveData(value: CGFloat) {
+        NSLog("lumu Manager Did ReceiveData %f", value);
+    }
+
+    func lumuManagerDidStopLumu() {
+        NSLog("lumu Manager Did Stop Lumu");
+    }
 }
 
